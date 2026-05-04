805ab4bdb45bfe000bc6f3ed2c3ec476c091d578
LJ1319 <luka.jikia.2@btu.edu.ge>
2026-05-04 21:07:08 +0400

    port

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,3 +1,13 @@
+import type { User } from '@/types/auth';
+
 export type * from './auth';
 export type * from './navigation';
 export type * from './ui';
+
+export interface Puppy {
+    id: number;
+    name: string;
+    trait: string;
+    imageUrl: string;
+    likedBy: User['id'][];
+}
